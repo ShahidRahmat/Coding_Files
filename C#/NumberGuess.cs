6324fe933e7e3f51df4e3b735655023afcd592cc
Shahid Rahmat <76436222+ShahidRahmat@users.noreply.github.com>
2021-06-13 01:18:32 +0800

    Create NumberGuess.cs

---
+++ C#/NumberGuess.cs
@@ -0,0 +1,22 @@
+using System;
+					
+public class NumberGuess
+{
+	public static void Main()
+	{
+		Random r = new Random();
+		
+		int ranNum = r.Next(0,100);
+		bool outcome = false;
+		
+		while(outcome == false) {
+			Console.WriteLine("Enter a number between 0 and 100: ");
+			string n = Console.ReadLine();
+			int num = int.Parse(n);
+			if (num == ranNum) {
+				Console.WriteLine("You have guessed the number!");
+				outcome = true;
+		}
+			else if (num > ranNum) {
+				Console.WriteLine("Too high, guess a lower number.");
+		}
